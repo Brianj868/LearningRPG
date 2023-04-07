5520b9b0cd67890627511c3fb0c3a8a057f2fbf5
Brian Johnston <brianj868@yahoo.com>
2023-04-06 20:22:30 -0700

    Added dialog giver

--- Assets/Scripts/UI/DialogController.cs
+++ Assets/Scripts/UI/DialogController.cs
@@ -9,16 +9,15 @@ using UnityEngine.UI;
 
 public class DialogController : MonoBehaviour
 {
-    [SerializeField] TextAsset _dialog;
     [SerializeField] TMP_Text _storyText;
     [SerializeField] Button[] _choiceButtons;
 
     Story _story;
 
     [ContextMenu("Start Dialog")]
-    void StartDialog()
+    public void StartDialog(TextAsset dialog)
     {
-        _story = new Story(_dialog.text);
+        _story = new Story(dialog.text);
         RefreshView();
     }
 
